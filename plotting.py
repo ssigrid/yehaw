57d6206bad1be3b9a826f2d1a65087d1adc83cc9
Sigrid Sysmäläinen <sigrid.sysmalainen@helsinki.fi>
2021-02-22 09:14:07 +0200

    final fixes on plotting before adding it to the main program

--- plotting.py
+++ plotting.py
@@ -1,8 +1,7 @@
 ### charts the words in files, and shows have many times which of given words has been used
 ### in which files/articles/books
 
-## seperate all the words in each articles
-## count how many times each word is there
+
 ## create planning which shows how many times which word is in the books that have the most of them
     ## should the lenght's of the books be considered?
     ## ready selected words or play as you wish
@@ -10,12 +9,24 @@
 ## words to look at in the final project: cowboy, horse, brave, pistol, lasso, cow, dog, milk, 
 ## whiskey/beer/scoths/alcohol, god, bible
 test_set = [["list", "in", "a", "list"], ["hope", "this", "this", "it", "it", "does", "it"], ["should", "i", "take", "a", "break"], ["i", "am", "clad", "this", "is", "this", "not", "usually", "manual"], ["can", "this", "i", "get", "a", "yeehaw"]]
-def seaborn(freq_dict):
+def seaborn(matches):
     import numpy as np
     import seaborn as sns
-    import matplotlib.pylot as plt
+    import matplotlib.pyplot as plt
     sns.set_theme(style="white", context="talk")
-    rs = np.randon.RandomState
+
+    x = []
+    y = []
+    for pair in matches:
+        x.append(pair[0])
+        y.append(pair[1])
+    
+    sns.barplot(x, y)
+    sns.color_palette("rocket")
+    plt.xticks(rotation=45)
+    plt.show()
+    
+    
 def wordfreq(articles):
     dict_articles = []
     for article in articles: 
@@ -28,6 +39,7 @@ def wordfreq(articles):
         dict_articles.append(dictionary)
     return dict_articles
 
+
 def freqhits(wordfreqs, streotype):
     matches = {}
     book_name = 0 ## merkkaa tällä hetkellä sitä mikä sit ois kirjan nimi, nyt vaan numero
@@ -35,7 +47,7 @@ def freqhits(wordfreqs, streotype):
         for key in dictio:
             if streotype == key:
                 value = dictio[key]
-                matches[book_name] = [value]
+                matches[book_name] = value
         book_name += 1
     sorted_matches = sorted(matches.items(), key=lambda x: x[1], reverse=True)
     if len(sorted_matches) > 10:
@@ -45,4 +57,4 @@ def freqhits(wordfreqs, streotype):
 dictionary = wordfreq(test_set)
 freggies = freqhits(dictionary, "this")
 #print(dictionary)
-print(freggies)
+seaborn(freggies)
