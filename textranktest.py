e738a30e9d5d04ebefdd76d4ad329981955ffcf8
Viima <Viima>
2021-03-02 14:32:17 +0200

    Added a txt and python file to test TextRank (specifically SingleRank atm)

---
+++ textranktest.py
@@ -0,0 +1,30 @@
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+import re
+import pke
+
+def read_file(file):
+
+    openfile = open(file, "r", encoding='utf-8')
+    readfile = openfile.read()
+    #filelist = readfile.split("</article>")
+    #filelist = filelist[99]                 
+    filelist = ''.join(readfile)
+    openfile.close()
+    
+
+    return filelist
+	
+wiki = "testdoc.txt"
+wikidoc = read_file(wiki)
+	
+pos = {'PROPN'}
+extractor = pke.unsupervised.SingleRank()
+extractor.load_document(input=wikidoc, language='en', normalization=None)
+extractor.candidate_selection(pos=pos)
+extractor.candidate_weighting(window=10, pos=pos)
+keyphrases = extractor.get_n_best(n=10)
+
+print(keyphrases)
